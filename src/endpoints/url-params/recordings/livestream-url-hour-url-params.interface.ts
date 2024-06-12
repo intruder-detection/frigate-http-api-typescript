7a5b3b77071da87dc282b3e37344e73b9a3b48a4
Rui Alves Personal <alvesrui951@gmail.com>
2024-06-12 16:39:17 +0100

    Added more endpoint responses

--- src/endpoints/url-params/recordings/livestream-url-hour-url-params.interface.ts
+++
@@ -1,9 +0,0 @@
-import { CameraNameUrlParams } from '../camera-name-url-params.interface';
-
-export interface LivestreamUrlHourUrlParamsInterface extends CameraNameUrlParams {
-  year: number;
-  month: number;
-  day: number;
-  hour: number;
-  timezone: string;
-}
--- src/endpoints/url-params/recordings/vod-specific-hour-url-params.interface.ts
+++ src/endpoints/url-params/recordings/vod-specific-hour-url-params.interface.ts
@@ -1,9 +1,10 @@
 import { CameraNameUrlParams } from '../camera-name-url-params.interface';
 
-export interface MetaDataForPreviewsInHourUrlParams extends CameraNameUrlParams {
+export interface VodSpecificHourUrlParams extends CameraNameUrlParams {
   year: number;
   month: number;
   day: number;
   hour: number;
+  // There's a specific endpoint that accepts the timezone
   timezone?: string;
 }
